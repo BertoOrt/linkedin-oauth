5c7759187b28cc5f12c31d87d621e4a4fa0ad4f0
Roberto Ortega <berto.ort@gmail.com>
2015-07-09 14:08:42 -0600

    api call to see picture

--- routes/auth.js
+++ routes/auth.js
@@ -5,13 +5,13 @@ var passport = require('passport');
 router.get('/auth/linkedin', passport.authenticate('linkedin'));
 
 router.get('/logout', function (req, res, next) {
-  req.session = null
-  res.redirect('/')
+  req.session = null;
+  res.redirect('/');
 });
 
 router.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
-    failureRedirect: '/',
-    successRedirect: '/'
-  }));
+  failureRedirect: '/',
+  successRedirect: '/'
+}));
 
 module.exports = router;
